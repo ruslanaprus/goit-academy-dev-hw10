a9bc95f64ba83df770020a6bcf2d0c7699b5aa7c
ruslana <ruslana.prus@ukr.net>
2024-10-10 21:55:50 +0200

    use ZonedDateTime and StringBuilder

--- src/main/java/org/example/service/TimeResponseBuilder.java
+++ src/main/java/org/example/service/TimeResponseBuilder.java
@@ -1,23 +1,24 @@
 package org.example.service;
 
+import org.apache.commons.text.StringEscapeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class TimeResponseBuilder {
     private static final Logger logger = LoggerFactory.getLogger(TimeResponseBuilder.class);
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public String buildResponse(ZoneId zoneId) {
-        OffsetDateTime localTimeWithOffset = OffsetDateTime.now(zoneId);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String formattedTime = localTimeWithOffset.format(formatter);
-
-        ZoneOffset offset = localTimeWithOffset.getOffset();
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(zoneId);
+        String formattedTime = zonedDateTime.format(FORMATTER);
+        ZoneOffset offset = zonedDateTime.getOffset();
         String formattedTimeWithZone = formattedTime + " UTC" + offset.getId();
+
         logger.info("Formatted time: {}", formattedTimeWithZone);
 
         return generateHtmlResponse(formattedTimeWithZone, zoneId);
@@ -25,13 +26,17 @@ public class TimeResponseBuilder {
 
     private String generateHtmlResponse(String formattedTime, ZoneId zoneId) {
         StringBuilder responseHtml = new StringBuilder();
-        responseHtml.append("<html><head><title>Current Time</title></head><body>")
-                .append("<h1>Current Time in ").append(zoneId.getId()).append("</h1>")
-                .append("<p>The current time is: <strong>")
-                .append(formattedTime)
-                .append("</strong></p>")
-                .append("</body></html>");
-
+        responseHtml.append("<html>")
+                .append("<head><title>Current Time</title></head>")
+                .append("<body>")
+                .append("<h1>Current Time in ").append(escapeHtml(zoneId.getId())).append("</h1>")
+                .append("<p>The current time is: <strong>").append(escapeHtml(formattedTime)).append("</strong></p>")
+                .append("</body>")
+                .append("</html>");
         return responseHtml.toString();
+    }
+
+    private String escapeHtml(String input) {
+        return input == null ? "" : StringEscapeUtils.escapeHtml4(input);
     }
 }
